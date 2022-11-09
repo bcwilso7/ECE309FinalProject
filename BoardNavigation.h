6b31a80ab03f83d502a04d9793fe9b1e9c93d52d
Bryan Wilson <bryanwilsonc@gmail.com>
2022-11-09 14:31:58 -0500

    added headers to project file

---
+++ BoardNavigation.h
@@ -0,0 +1,14 @@
+//
+// Created by Bryan Wilson on 11/9/2022.
+//
+
+#ifndef FINALPROJECT_BOARDNAVIGATION_H
+#define FINALPROJECT_BOARDNAVIGATION_H
+
+
+class BoardNavigation {
+
+};
+
+
+#endif //FINALPROJECT_BOARDNAVIGATION_H
---
+++ RulesandPlay.h
@@ -0,0 +1,14 @@
+//
+// Created by Bryan Wilson on 11/9/2022.
+//
+
+#ifndef FINALPROJECT_RULESANDPLAY_H
+#define FINALPROJECT_RULESANDPLAY_H
+
+
+class RulesandPlay {
+
+};
+
+
+#endif //FINALPROJECT_RULESANDPLAY_H
